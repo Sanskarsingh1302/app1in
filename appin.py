10350f7df138f34140167758c51f531820e6ea1f
Sanskarsingh1302 <sanskarsingh725@gmail.com>
2025-03-16 16:57:25 +0530

    appin

--- appin.py
+++ appin.py
@@ -123,7 +123,7 @@ if app_mode == "Know your terrain":
 
     if uploaded_file is not None:
         image = Image.open(uploaded_file)
-        st.image(image, caption='Uploaded Image', use_column_width=True)
+        st.image(image, caption='Uploaded Image', use_container_width=True)
         st.header("Classified image as:", divider=True)
 
         # Make the prediction
